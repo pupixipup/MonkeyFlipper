2c693baa6f790b7420533a9e73ccba5d7e3c10ad
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-05-26 13:15:25 +0200

    fix: jumping direction, feat: add sliding

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,59 +2,45 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -270.0
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var raycast = $RayCast2D
 var onWall = false
-var lookDirection = 1
+var jumpDirection = 1
 const RAY_RIGHT = Vector2(6,0)
 const RAY_LEFT = Vector2(-7,0)
 const MAX_JUMPS = 2
 var jumps_left = MAX_JUMPS
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var direction = 1 # Move right by default.  Input.get_axis("move_left", "move_right")
 var previousDirection = null
 
 func _physics_process(delta):
-	# Add the gravity.
-	var direction = Input.get_axis("move_left", "move_right")
 
 	var collider = raycast.get_collider()
-	var reachedWall = collider and collider.get_class() == "StaticBody2D"
-	if reachedWall:
-		onWall = collider.get_collision_layer() == 5
-	else:
-		onWall = false	
+	var onWall = collider and collider.get_class() == "StaticBody2D" and collider.get_collision_layer() == 5
+
 	if not is_on_floor():
 		if onWall:
-			velocity.y = 0
+			velocity.y = 2
 		else:
 			velocity.y += gravity * delta
-
-	# Handle jump.
-	
-		#elif is_on_floor():
-			#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-
 	
 	if onWall:
 		jumps_left = MAX_JUMPS
 		animated_sprite.play("idle")
 	else:
+		# Moving
 		if direction:
 			if is_on_floor():
 				animated_sprite.play("move")
-				if direction != 0:
-					lookDirection = direction
-					print("-->", lookDirection)
+				jumps_left = MAX_JUMPS
+
 			else:
 				animated_sprite.play("jump")
-			velocity.x = direction * SPEED
+			velocity.x = jumpDirection * SPEED
+			# set visual direction
 			if direction == -1:
 				animated_sprite.flip_h = true
 				raycast.target_position = RAY_LEFT
@@ -62,28 +48,36 @@ func _physics_process(delta):
 			else:
 				animated_sprite.flip_h = false
 				raycast.target_position = RAY_RIGHT
-		elif is_on_floor():
-			animated_sprite.play("idle")
-			jumps_left = MAX_JUMPS
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		# Idle
+		#elif is_on_floor():
+		#	animated_sprite.play("idle")
+		#	jumps_left = MAX_JUMPS
+		#	velocity.x = move_toward(velocity.x, 0, SPEED)
 			
 			
 	if Input.is_action_just_pressed("ui_accept"):
+
+
 			if jumps_left > 0:
+				if is_on_floor() == false:
+					jumpDirection = jumpDirection * -1
+					direction = direction  * -1
+				
 				animated_sprite.play("jump")
-				var lookRight = lookDirection == 1
-				velocity.x = lookDirection * SPEED
+				var lookRight = jumpDirection == 1
+	
+				velocity.x = jumpDirection * SPEED
+ 
 				velocity.y = JUMP_VELOCITY
 				velocity.y += gravity * delta
 				animated_sprite.flip_h = !lookRight
 				raycast.target_position = RAY_RIGHT if lookRight  else RAY_LEFT
-				
-				lookDirection = lookDirection * -1
+			
+		
 
 			jumps_left -= 1
 
 	move_and_slide()
+	
 
-
-func _on_ground_body_entered(body):
-	pass # Replace with function body.
